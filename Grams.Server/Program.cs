65f446978d284b13ee2a7bf8caf4f33429aff037
eddyiiz <eddyxsayf@gmail.com>
2024-10-23 14:07:02 +0200

    add db integration

--- Grams.Server/Program.cs
+++ Grams.Server/Program.cs
@@ -1,3 +1,4 @@
+global using Grams.Server.Data;
 global using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//db integration
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
