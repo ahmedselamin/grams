4692ff66a389070173ed642974bc14c923650f52
eddyiiz <eddyxsayf@gmail.com>
2024-10-23 14:13:02 +0200

    add service response model

---
+++ Grams.Server/Models/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Grams.Server.Models;
+
+public class ServiceResponse<T>
+{
+    public T? Data { get; set; }
+    public string Message { get; set; } = "Success";
+    public bool Success { get; set; } = true;
+
+}
