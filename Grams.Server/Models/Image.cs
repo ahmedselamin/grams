09ebbbf217ab87cc9709ca729f04dee89d44c310
eddyiiz <eddyxsayf@gmail.com>
2024-10-23 15:51:51 +0200

    add image model

---
+++ Grams.Server/Models/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace Grams.Server.Models;
+
+public class Image
+{
+    public int Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
