af6714841505696385fcc2b7fd51180122580f0d
eddyiiz <eddyxsayf@gmail.com>
2024-10-23 20:20:55 +0200

    register post service

--- Grams.Server/Program.cs
+++ Grams.Server/Program.cs
@@ -2,6 +2,7 @@ global using Grams.Server.Data;
 global using Microsoft.EntityFrameworkCore;
 using Grams.Server.Services.AuthService;
 using Grams.Server.Services.ImageService;
+using Grams.Server.Services.PostService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 //add services to DI scope
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<IPostService, PostService>();
 
 var app = builder.Build();
 
