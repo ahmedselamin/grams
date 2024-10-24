e7f3098bf3a1d9022e72caf2ba3e17e14a7ea3b0
eddyiiz <eddyxsayf@gmail.com>
2024-10-24 11:42:08 +0200

    users table: add password salt

--- Grams.Server/Migrations/20241024093513_UpdateUsers.Designer.cs
+++ Grams.Server/Migrations/20241024093513_UpdateUsers.Designer.cs
@@ -3,6 +3,7 @@ using Grams.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Grams.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241024093513_UpdateUsers")]
+    partial class UpdateUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
