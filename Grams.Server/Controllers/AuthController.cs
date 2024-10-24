1042a2bf53c5a9455b2124e50c754932aab94af1
eddyiiz <eddyxsayf@gmail.com>
2024-10-24 10:57:00 +0200

    register route

---
+++ Grams.Server/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grams.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> Register([FromBody] UserRegister request)
+    {
+        var response = await _authService
+            .Register(new User
+            {
+                Username = request.Username,
+            }, request.Password);
+
+        return response.Success ? Ok(response) : BadRequest(response);
+    }
+}
