c246d5a28fabf47a4cd3ebf8c0be4d32909dcb35
eddyiiz <eddyxsayf@gmail.com>
2024-10-24 10:28:27 +0200

    update model

--- Grams.Server/Models/User.cs
+++ Grams.Server/Models/User.cs
@@ -1,7 +1,10 @@
-﻿namespace Grams.Server.Models;
+﻿
+namespace Grams.Server.Models;
 
 public class User
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; } = new byte[32];
