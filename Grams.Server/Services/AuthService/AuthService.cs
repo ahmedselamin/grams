6f1b675f199360b2f50e7223f112acabe407d3e2
eddyiiz <eddyxsayf@gmail.com>
2024-10-24 11:42:24 +0200

    debug login

--- Grams.Server/Services/AuthService/AuthService.cs
+++ Grams.Server/Services/AuthService/AuthService.cs
@@ -29,9 +29,10 @@ public class AuthService : IAuthService
                 return response;
             }
 
-            CreatePasswordHash(password, out byte[] passwordHash);
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
 
             user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -64,20 +65,18 @@ public class AuthService : IAuthService
 
                 return response;
             }
-            if (!VerifyPassword(password, user.PasswordHash))
+            if (!VerifyPassword(password, user.PasswordHash, user.PasswordSalt))
             {
                 response.Success = false;
                 response.Message = "Wrong password";
 
                 return response;
             }
-            else
-            {
-                response.Data = CreateToken(user);
-                response.Message = $"Welcome back, ${user.Username}";
 
-                return response;
-            }
+            response.Data = CreateToken(user);
+            response.Message = $"Welcome back, {user.Username}";
+
+            return response;
 
         }
         catch (Exception ex)
@@ -123,18 +122,21 @@ public class AuthService : IAuthService
         );
     }
 
-    private bool VerifyPassword(string password, byte[] passwordHash)
+    private bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt)
     {
-        var hmac = new HMACSHA512();
-
-        var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-
-        return computeHash.SequenceEqual(passwordHash);
+        using (var hmac = new HMACSHA512(passwordSalt))
+        {
+            var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computeHash.SequenceEqual(passwordHash);
+        }
     }
 
-    private void CreatePasswordHash(string password, out byte[] passwordHash)
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-        var hmac = new HMACSHA512();
-        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
     }
 }
