c1e07773b6a4c55a9760edece07d91788a4f5bcf
eddyiiz <eddyxsayf@gmail.com>
2024-10-25 10:43:01 +0200

    update

--- Grams.Server/Services/ImageService/IImageService.cs
+++ Grams.Server/Services/ImageService/IImageService.cs
@@ -2,7 +2,7 @@
 
 public interface IImageService
 {
-    Task<ServiceResponse<List<Image>>> GetImages(int userId);
+    Task<ServiceResponse<List<Image>>> GetImages();
     Task<ServiceResponse<Image>> UploadImage(int userId, IFormFile file);
     Task<ServiceResponse<bool>> DeleteImage(int imageId);
 }
