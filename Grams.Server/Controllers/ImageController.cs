915ca36e4e5118ad31b5ab90207450ae6901c885
eddyiiz <eddyxsayf@gmail.com>
2024-10-25 09:54:58 +0200

    add upload route

---
+++ Grams.Server/Controllers/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Grams.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ImageController : ControllerBase
+{
+    private readonly IImageService _imageService;
+
+    public ImageController(IImageService imageService)
+    {
+        _imageService = imageService;
+    }
+
+    [HttpPost("upload"), Authorize]
+    public async Task<ActionResult> UploadImage([FromBody] IFormFile file)
+    {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var response = await _imageService.UploadImage(userId, file);
+
+        return response.Success ? Ok(response) : BadRequest(response);
+    }
+}
