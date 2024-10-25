fe74e13a022ac14552bf3a49f82f376dba367fac
eddyiiz <eddyxsayf@gmail.com>
2024-10-25 10:32:26 +0200

    update model

--- Grams.Server/Models/Image.cs
+++ Grams.Server/Models/Image.cs
@@ -7,6 +7,7 @@ public class Image
     public int Id { get; set; }
     public int UserId { get; set; }
     public string FilePath { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
     public string ContentType { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
