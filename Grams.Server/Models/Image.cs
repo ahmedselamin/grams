82a4a2dfb4c75e0dad0fd99f4496f3570a969551
eddyiiz <eddyxsayf@gmail.com>
2024-10-25 09:40:32 +0200

    add user id

--- Grams.Server/Models/Image.cs
+++ Grams.Server/Models/Image.cs
@@ -2,7 +2,10 @@
 
 public class Image
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    public int UserId { get; set; }
     public string FilePath { get; set; } = string.Empty;
     public string ContentType { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
