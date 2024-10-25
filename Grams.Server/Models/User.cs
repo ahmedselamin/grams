54ecd6ed4478075bdb70c35a41ebae0b89639e73
eddyiiz <eddyxsayf@gmail.com>
2024-10-25 09:40:22 +0200

    add images

--- Grams.Server/Models/User.cs
+++ Grams.Server/Models/User.cs
@@ -9,5 +9,5 @@ public class User
     public string Username { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; } = new byte[32];
     public byte[] PasswordSalt { get; set; } = new byte[32];
-
+    public List<Image> Images { get; set; } = new List<Image>();
 }
