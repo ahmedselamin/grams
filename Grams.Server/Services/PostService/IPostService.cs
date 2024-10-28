d835a66282cc991b733a5f611b0024a6ccdafddb
eddyiiz <eddyxsayf@gmail.com>
2024-10-28 10:15:33 +0200

    add interface

--- Grams.Server/Services/PostService/IPostService.cs
+++ Grams.Server/Services/PostService/IPostService.cs
@@ -2,4 +2,9 @@
 
 public interface IPostService
 {
+    Task<ServiceResponse<List<Post>>> GetPosts();
+    Task<ServiceResponse<Post>> GetPost(int PostId);
+    Task<ServiceResponse<Post>> AddPost(int userId, Post post);
+    Task<ServiceResponse<Post>> UpdatePost(int userId, int postId, Post updatedPost);
+    Task<ServiceResponse<bool>> DeletePost(int userId, int postId);
 }
