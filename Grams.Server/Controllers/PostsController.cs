32be5a8b50ba70ac3f48052bce36812ddb8df6b8
eddyiiz <eddyxsayf@gmail.com>
2024-10-28 10:26:14 +0200

    add Fetch Posts

---
+++ Grams.Server/Controllers/PostsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grams.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PostsController : ControllerBase
+{
+    private readonly IPostService _postService;
+
+    public PostsController(IPostService postService)
+    {
+        _postService = postService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> FetchPosts()
+    {
+        var response = await _postService.GetPosts();
+        return response.Success ? Ok(response) : BadRequest(response.Message);
+    }
+}
