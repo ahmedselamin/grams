0f639eb06c7df00b3e7e6eff2c35f0583418e690
eddyiiz <eddyxsayf@gmail.com>
2024-10-28 11:44:24 +0200

    Fix post model

--- Grams.Server/Models/Post.cs
+++ Grams.Server/Models/Post.cs
@@ -6,7 +6,9 @@ public class Post
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public int UserId { get; set; }
-    public int ImageId { get; set; }
+    public string FilePath { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
     public string? Caption { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
