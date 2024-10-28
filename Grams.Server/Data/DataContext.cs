b1f96b7851464eb9406433409c28e0910d03ee73
eddyiiz <eddyxsayf@gmail.com>
2024-10-28 11:45:17 +0200

    post & user

--- Grams.Server/Data/DataContext.cs
+++ Grams.Server/Data/DataContext.cs
@@ -3,16 +3,10 @@
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
-    public DbSet<Image> Images { get; set; }
     public DbSet<Post> Posts { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>()
-            .HasMany(u => u.Images)
-            .WithOne()
-            .HasForeignKey(i => i.UserId);
-
         modelBuilder.Entity<User>()
             .HasMany(u => u.Posts)
             .WithOne()
