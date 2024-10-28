541163bf2325f0d6d54bf108ea3613ea24439e24
eddyiiz <eddyxsayf@gmail.com>
2024-10-28 09:54:11 +0200

    add post model

---
+++ Grams.Server/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace Grams.Server.Models;
+
+public class Post
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int ImageId { get; set; }
+    public string? Caption { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
