36b9db8687021f2dde5b031ed9fdfa5312194efb
eddyiiz <eddyxsayf@gmail.com>
2024-10-28 11:49:29 +0200

    debug AddPost

--- Grams.Server/Services/PostService/IPostService.cs
+++ Grams.Server/Services/PostService/IPostService.cs
@@ -4,7 +4,7 @@ public interface IPostService
 {
     Task<ServiceResponse<List<Post>>> GetPosts();
     Task<ServiceResponse<Post>> GetPost(int PostId);
-    Task<ServiceResponse<Post>> AddPost(int userId, Post post);
+    Task<ServiceResponse<Post>> AddPost(int userId, IFormFile file, string? caption);
     Task<ServiceResponse<Post>> UpdatePost(int userId, int postId, Post updatedPost);
     Task<ServiceResponse<bool>> DeletePost(int userId, int postId);
 }
