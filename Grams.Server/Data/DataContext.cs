dc97ffaf9895a2c3fb0acb91f21bfca22aa1ae90
eddyiiz <eddyxsayf@gmail.com>
2024-10-29 09:54:01 +0200

    fix

--- Grams.Server/Data/DataContext.cs
+++ Grams.Server/Data/DataContext.cs
@@ -3,7 +3,8 @@
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
-    { }
+    {
+    }
     public DbSet<User> Users { get; set; }
     public DbSet<Post> Posts { get; set; }
     public DbSet<Like> Likes { get; set; }
