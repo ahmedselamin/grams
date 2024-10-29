b8e006052df6b346f97484e472a7157e551d1027
eddyiiz <eddyxsayf@gmail.com>
2024-10-29 09:38:54 +0200

    add like model

---
+++ Grams.Server/Models/Like.cs
@@ -0,0 +1,11 @@
+﻿namespace Grams.Server.Models;
+
+public class Like
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int PostId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
