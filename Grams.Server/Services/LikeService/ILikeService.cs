e490ab26b69920d56bc9b0bbd99e0c7f674d7017
eddyiiz <eddyxsayf@gmail.com>
2024-10-29 10:30:24 +0200

    add interface

--- Grams.Server/Services/LikeService/ILikeService.cs
+++ Grams.Server/Services/LikeService/ILikeService.cs
@@ -2,4 +2,8 @@
 
 public interface ILikeService
 {
+    Task<ServiceResponse<int>> GetLikesCount(int postId);
+    Task<ServiceResponse<bool>> LikePost(int userId, int postId);
+    Task<ServiceResponse<bool>> DislikePost(int userId, int postId);
+    Task<ServiceResponse<bool>> IsLiked(int userId, int postId);
 }
