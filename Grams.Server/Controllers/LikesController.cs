133916364d7e4a5c31493211542167d8e3ccf5f8
eddyiiz <eddyxsayf@gmail.com>
2024-10-29 13:52:26 +0200

    add get likes count

---
+++ Grams.Server/Controllers/LikesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grams.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LikesController : ControllerBase
+{
+    private readonly ILikeService _likeService;
+
+    public LikesController(ILikeService likeService)
+    {
+        _likeService = likeService;
+    }
+
+    [HttpGet("count/{id:int}")]
+    public async Task<ActionResult> GetLikesCount(int id)
+    {
+        var response = await _likeService.GetLikesCount(id);
+
+        return response.Success ? Ok(response) : BadRequest(response.Message);
+    }
+}
