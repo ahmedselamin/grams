30fd9c36fa345d58c82a4407f6765285015fe38c
eddyiiz <eddyxsayf@gmail.com>
2024-10-29 08:40:53 +0200

    debug update post

--- Grams.Server/Services/PostService/IPostService.cs
+++ Grams.Server/Services/PostService/IPostService.cs
@@ -5,6 +5,6 @@ public interface IPostService
     Task<ServiceResponse<List<Post>>> GetPosts();
     Task<ServiceResponse<Post>> GetPost(int PostId);
     Task<ServiceResponse<Post>> AddPost(int userId, IFormFile file, string? caption);
-    Task<ServiceResponse<Post>> UpdatePost(int userId, int postId, Post updatedPost);
+    Task<ServiceResponse<Post>> UpdatePost(int userId, int postId, string updatedCaption);
     Task<ServiceResponse<bool>> DeletePost(int userId, int postId);
 }
