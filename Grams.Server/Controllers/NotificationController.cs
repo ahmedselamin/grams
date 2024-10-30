9fae77df85fe88ab1c705ed308fbe075ced262dd
eddyiiz <eddyxsayf@gmail.com>
2024-10-30 13:07:29 +0200

    add fetch notifications

---
+++ Grams.Server/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Grams.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NotificationController : ControllerBase
+{
+    private readonly INotificationService _notificationService;
+
+    public NotificationController(INotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    [HttpGet, Authorize]
+    public async Task<ActionResult> FetchNotifications()
+    {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var response = await _notificationService.GetNotifications(userId);
+
+        return Ok(response);
+    }
+}
