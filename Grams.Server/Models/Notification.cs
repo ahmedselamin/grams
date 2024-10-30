fbe024923352edde64ae0d5ce4709b70faf964f2
eddyiiz <eddyxsayf@gmail.com>
2024-10-30 11:39:49 +0200

    add notification model

---
+++ Grams.Server/Models/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace Grams.Server.Models;
+
+public class Notification
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
