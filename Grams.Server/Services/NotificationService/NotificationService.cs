8d752790708de8d591ad03790236e8d687b034e3
eddyiiz <eddyxsayf@gmail.com>
2024-10-30 12:21:29 +0200

    add get notifications

--- Grams.Server/Services/NotificationService/NotificationService.cs
+++ Grams.Server/Services/NotificationService/NotificationService.cs
@@ -1,5 +1,58 @@
-﻿namespace Grams.Server.Services.NotificationService;
+﻿
+namespace Grams.Server.Services.NotificationService;
 
 public class NotificationService : INotificationService
 {
+    private readonly DataContext _context;
+
+    public NotificationService(DataContext context)
+    {
+        _context = context;
+    }
+    public async Task<ServiceResponse<List<Notification>>> GetNotifications(int userId)
+    {
+        var response = new ServiceResponse<List<Notification>>();
+
+        try
+        {
+            var notifications = await _context.Notifications
+                .Where(n => n.UserId == userId)
+                .OrderByDescending(n => n.Id)
+                .ToListAsync();
+            if (!notifications.Any())
+            {
+                response.Success = false;
+                response.Message = "No notifications";
+
+                return response;
+            }
+
+            response.Data = notifications;
+            return response;
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+
+            return response;
+        }
+    }
+
+    public async Task<ServiceResponse<bool>> SendNotification(int userId, Notification notification)
+    {
+        var response = new ServiceResponse<bool>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+
+            return response;
+        }
+    }
 }
