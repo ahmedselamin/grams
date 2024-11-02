6d24ecd6c587f984c56f3821196361c03af3db6f
eddyiiz <eddyxsayf@gmail.com>
2024-11-02 10:35:09 +0200

    add comment model

---
+++ Grams.Server/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Grams.Server.Models;
+
+public class Comment
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public int PostId { get; set; }
+    public int UserId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
