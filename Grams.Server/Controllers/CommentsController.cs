0f9b19d4e9e459467fbbe0d1b6e7ae99c932edbc
eddyiiz <eddyxsayf@gmail.com>
2024-11-02 17:17:58 +0200

    add fetch comments

---
+++ Grams.Server/Controllers/CommentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grams.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommentsController : ControllerBase
+{
+    private readonly CommentService _commentService;
+
+    public CommentsController(CommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpGet("fetch-comments")]
+    public async Task<ActionResult> FetchComments(int postId)
+    {
+        var response = await _commentService.GetComments(postId);
+
+        return response.Success ? Ok(response) : BadRequest(response);
+    }
+
+}
