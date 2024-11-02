f9b9c90ace0abab891e9da2a2e92d2bca51bb702
eddyiiz <eddyxsayf@gmail.com>
2024-11-02 16:46:36 +0200

    add get comments

--- Grams.Server/Services/CommentService/CommentService.cs
+++ Grams.Server/Services/CommentService/CommentService.cs
@@ -1,5 +1,102 @@
-﻿namespace Grams.Server.Services.CommentService;
+﻿
+namespace Grams.Server.Services.CommentService;
 
 public class CommentService : ICommentService
 {
+    private readonly DataContext _context;
+
+    public CommentService(DataContext context)
+    {
+        _context = context;
+    }
+    public async Task<ServiceResponse<List<Comment>>> GetComments(int postId)
+    {
+        var response = new ServiceResponse<List<Comment>>();
+
+        try
+        {
+            var comments = await _context.Comments
+                .OrderByDescending(c => c.CreatedAt)
+                .ToListAsync();
+
+            response.Data = comments;
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+
+            return response;
+        }
+    }
+
+    public async Task<ServiceResponse<Comment>> GetComment(int commentId)
+    {
+        var response = new ServiceResponse<Comment>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+
+            return response;
+        }
+    }
+
+    public async Task<ServiceResponse<Comment>> CreateComment(int userId, int postId, string content)
+    {
+        var response = new ServiceResponse<Comment>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+
+            return response;
+        }
+    }
+
+    public async Task<ServiceResponse<Comment>> UpdateComment(int userId, int postId, string updatedContent)
+    {
+        var response = new ServiceResponse<Comment>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+
+            return response;
+        }
+    }
+
+    public async Task<ServiceResponse<bool>> DeleteComment(int userId, int commentId)
+    {
+        var response = new ServiceResponse<bool>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+
+            return response;
+        }
+    }
 }
