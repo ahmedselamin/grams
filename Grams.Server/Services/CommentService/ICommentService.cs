ff7c0bc567bc069e89204085a802cb0d520c3334
eddyiiz <eddyxsayf@gmail.com>
2024-11-02 16:38:09 +0200

    add  comment service

--- Grams.Server/Services/CommentService/ICommentService.cs
+++ Grams.Server/Services/CommentService/ICommentService.cs
@@ -2,4 +2,9 @@
 
 public interface ICommentService
 {
+    Task<ServiceResponse<List<Comment>>> GetComments(int postId);
+    Task<ServiceResponse<Comment>> GetComment(int commentId);
+    Task<ServiceResponse<Comment>> CreateComment(int userId, int postId, string content);
+    Task<ServiceResponse<Comment>> UpdateComment(int userId, int postId, string updatedContent);
+    Task<ServiceResponse<bool>> DeleteComment(int userId, int commentId);
 }
