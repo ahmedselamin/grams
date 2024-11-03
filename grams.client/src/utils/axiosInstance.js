45b60cd59f4943915efcff5738a9176247451813
eddyiiz <eddyxsayf@gmail.com>
2024-11-04 00:22:07 +0200

    add axios instance

--- grams.client/src/utils/axiosInstance.js
+++ grams.client/src/utils/axiosInstance.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+import axios from 'axios';
+
+const axiosInstance = axios.Create({
+    const: "https://localhost:7267/api",
+});
+
+//request interceptor
+
+
+export default axiosInstance;
