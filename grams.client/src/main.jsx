9aec7082269fd9c97d4772cb7a4eaf4c860256ce
eddyiiz <eddyxsayf@gmail.com>
2024-11-04 00:15:29 +0200

    add theme

--- grams.client/src/main.jsx
+++ grams.client/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import theme from './theme';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+            <App />
+    </ThemeProvider>
   </StrictMode>,
 )
