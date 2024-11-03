6ee4c5c6ed4442b98c19e0817752ecbdca9b45cd
eddyiiz <eddyxsayf@gmail.com>
2024-11-03 06:11:52 +0200

    add get comments count

--- Grams.Server/Services/CommentService/ICommentService.cs
+++ Grams.Server/Services/CommentService/ICommentService.cs
@@ -3,6 +3,7 @@
 public interface ICommentService
 {
     Task<ServiceResponse<List<Comment>>> GetComments(int postId);
+    Task<ServiceResponse<int>> GetCommentsCount(int postId);
     Task<ServiceResponse<Comment>> GetComment(int commentId);
     Task<ServiceResponse<Comment>> CreateComment(int userId, int postId, string content);
     Task<ServiceResponse<Comment>> UpdateComment(int userId, int commentId, string updatedContent);
