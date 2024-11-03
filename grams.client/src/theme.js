3e3a9ee82ecbd4c2765a746275755e6220d3b346
eddyiiz <eddyxsayf@gmail.com>
2024-11-04 00:08:52 +0200

    add tyography styles

--- grams.client/src/theme.js
+++ grams.client/src/theme.js
@@ -19,4 +19,25 @@ const theme = createTheme({
             secondary: '#729c8c', // Greyish secondary text
         },
     },
+
+    typography: {
+        fontFamily: 'Roboto, Arial, sans-serif, Grey Qo',
+        h1: {
+            fontSize: '3rem',
+            fontWeight: 700,
+            letterSpacing: '0.1rem',
+            color: '#000',
+        },
+        h2: {
+            fontSize: '2rem',
+            fontWeight: 600,
+            letterSpacing: '0.05rem',
+            color: '#000',
+        },
+        button: {
+            textTransform: 'none',
+        },
+    },
 });
+
+export default theme;
