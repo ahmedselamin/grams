b871f2dd45165ed266d8c3f8278e53a196bd4044
eddyiiz <eddyxsayf@gmail.com>
2024-11-04 10:19:29 +0200

    init

--- grams.client/src/component/navbar.jsx
+++ grams.client/src/component/navbar.jsx
@@ -16,51 +16,7 @@ const Navbar = () => {
     const menuItems = ['Home', 'Profile', 'Settings'];
 
     return (
-        <AppBar position="sticky" color="primary">
-            <Toolbar>
-                <Typography variant="h4" component="header" sx={{ flexGrow: 1 }}>
-                    Grams
-                </Typography>
-
-                {/* desktop menu */}
-                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                    {menuItems.map((item) => (
-                        <Button key={item} color="inherit">
-                            {item}
-                        </Button>
-                    ))}
-                </Box>
-
-                {/* Hamburger icon for smaller screens */}
-                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                    <IconButton edge="start" color="inherit" onClick={handleMenuOpen}>
-                        <MenuIcon />
-                    </IconButton>
-                </Box>
-            </Toolbar>
-
-            {/* Full-width dropdown menu for smaller screens */}
-            <Menu
-                anchorEl={anchor}
-                open={Boolean(anchor)}
-                onClose={handleMenuClose}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-                transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-                PaperProps={{
-                    sx: {
-                        mt: 2,
-                        width: '100%',
-                        maxWidth: '97%',
-                    },
-                }}>
-            
-                {menuItems.map((item) => (
-                    <MenuItem key={item} onClick={handleMenuClose} sx={{ justifyContent: 'center' }}>
-                        {item}
-                    </MenuItem>
-                ))}
-            </Menu>
-        </AppBar>
+        
     );
 };
 
