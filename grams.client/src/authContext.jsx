023831abe40df137daaf8832a4c344ff2c2980dd
eddyiiz <eddyxsayf@gmail.com>
2024-11-05 11:12:58 +0200

    add login

--- grams.client/src/authContext.jsx
+++ grams.client/src/authContext.jsx
@@ -23,4 +23,31 @@ export const AuthProvider = ({ children }) => {
             return false;
         }
     }
+
+    const login = async (formData) => {
+        try {
+            const response = await axiosInstance.post('/auth/login', {
+                username: formData.username,
+                password: formData.password
+            });
+
+            const token = response.data.token;
+            const decodedToken = jwtDecode(token);
+
+            const user = decodedToken['name'];
+
+            // Save to local storage and update state
+            localStorage.setItem('token', token);
+            localStorage.setItem('username', user);
+            setIsAuthenticated(true);
+            setUsername(user);
+
+            return true; 
+
+        } catch (error) {
+            console.error(error);
+
+            return false;
+        }
+    }
 }
