c4a515c284a50bbd4c244a1a81e891a0da5c69a5
eddyiiz <eddyxsayf@gmail.com>
2024-11-05 12:08:03 +0200

    init

--- grams.client/src/pages/home.jsx
+++ grams.client/src/pages/home.jsx
@@ -1,4 +1,36 @@
+import { useState, useEffect, useContext } from "react";
+import {
+    Card,
+    CardContent,
+    Typography,
+    Stack,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+    Container,
+} from "@mui/material";
+import axiosInstance from "../utils/axiosInstance";
+import { AuthContext } from "../authContext";
+
 const Home = () => {
+    const [isAuthenticated, setIsAuthenticated] = useContext(AuthContext);
+    const [grams, setGrams] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        file: null,
+        caption: ""
+    });
+
+    const handleInputChange = (e) => {
+        setFormData({
+            ...formData,
+
+        })
+    }
 
 };
 
