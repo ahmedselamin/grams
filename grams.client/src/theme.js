94dcb0e0b0edc20205cb3091559c6821406c3d70
eddyiiz <eddyxsayf@gmail.com>
2024-11-05 10:58:16 +0200

    update theme

--- grams.client/src/theme.js
+++ grams.client/src/theme.js
@@ -3,40 +3,14 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
     palette: {
         primary: {
-            main: "#005477"  // oceanic blue color 
+            main: '#005477',
         },
         secondary: {
-            main: "#f7fcfc"  // off white color
-        },
-        third: {
-            main: "#d32f2f"  // red for errors
-        },
-        background: {
-            default: '#fff', // White background
-        },
-        text: {
-            primary: '#000', // Black text
-            secondary: '#729c8c', // Greyish secondary text
+            main: '#f50057',
         },
     },
-
     typography: {
-        fontFamily: 'Roboto, Arial, sans-serif, Grey Qo',
-        h1: {
-            fontSize: '3rem',
-            fontWeight: 700,
-            letterSpacing: '0.1rem',
-            color: '#000',
-        },
-        h2: {
-            fontSize: '2rem',
-            fontWeight: 600,
-            letterSpacing: '0.05rem',
-            color: '#000',
-        },
-        button: {
-            textTransform: 'none',
-        },
+        fontFamily: 'Roboto, sans-serif',
     },
 });
 
