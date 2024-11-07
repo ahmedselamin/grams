8fd23ac4ef89e7c2ffb52f0cfc191bd08e7343d5
eddyiiz <eddyxsayf@gmail.com>
2024-11-07 12:25:21 +0200

    add fetch

--- grams.client/src/pages/home.jsx
+++ grams.client/src/pages/home.jsx
@@ -17,7 +17,36 @@ import axiosInstance from "../utils/axiosInstance";
 import { AuthContext } from "../authContext";
 
 const Home = () => {
-    
+    const [isAuthenticated] = useContext(AuthContext);
+    const [post, setPosts] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        caption: "",
+        photo: null,
+    });
+
+    const handleInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const clearForm = () => {
+        setFormData({
+            caption: "",
+            photo: null,
+        })
+    };
+
+    const fetchGrams = async () => {
+        try {
+            const response = await axiosInstance.get('/Posts/fetch');
+            setPosts(response.data.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <h2>Home</h2>
