7d553696313323ba8752e44ae9d869264af3affd
eddyiiz <eddyxsayf@gmail.com>
2024-11-07 11:40:10 +0200

    add cors

--- Grams.Server/Program.cs
+++ Grams.Server/Program.cs
@@ -86,6 +86,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//CORS
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+    options.AllowAnyOrigin();
+});
+
 app.UseAuthentication();
 
 app.UseAuthorization();
