7991339ab0fed1d49330806f64f7f35e16022b6b
eddyiiz <eddyxsayf@gmail.com>
2024-11-07 11:27:15 +0200

    debug

--- grams.client/src/component/navbar.jsx
+++ grams.client/src/component/navbar.jsx
@@ -2,11 +2,11 @@ import { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar, IconButton, Typography, Button, Menu, MenuItem, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { AuthContext } from '../AuthContext';
+import { AuthContext } from '../authContext';
 
 const Navbar = () => {
     const [anchorEl, setAnchorEl] = useState(null);
-    const [isAuthenticated, logout] = useContext(AuthContext);
+    const { isAuthenticated, logout } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleLogout = () => {
@@ -14,17 +14,19 @@ const Navbar = () => {
         navigate('/');
     };
 
-    const handleMenueOpen = (e) => {
-        setAnchorEl(e.currentTarget);
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
     };
 
-    const handleMenueClose = () => {
+
+    const handleMenuClose = () => {
         setAnchorEl(null);
     };
 
     return (
-        <AppBar position="sticky"
-            sx={{
+        <>
+            <AppBar position="sticky" sx={{
                 backgroundColor: '#005477',
                 color: 'white',
                 borderRadius: "40px",
@@ -32,79 +34,82 @@ const Navbar = () => {
                 maxWidth: '1500px',
                 padding: '10px 20px'
             }}>
-            <Toolbar>
-                <Typography
-                    variant="h4"
-                    component={Link}
-                    to="/"
-                    sx={{
-                        flexGrow: 1,
-                        textDecoration: 'none',
-                        color: 'white',
-                        fontWeight: 'bold',
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}>
-                    Grams
-                </Typography>
+                <Toolbar>
+                    <Typography
+                        variant="h4"
+                        component={Link}
+                        to="/"
+                        sx={{
+                            flexGrow: 1,
+                            textDecoration: 'none',
+                            color: 'white',
+                            fontWeight: 'bold',
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}>
+                        Grams
+                    </Typography>
 
-                {/ *  desktop*/}
-                <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-                    <Button component={Link} to="/" sx={{ color: 'white', fontWeight: 'bold' }}>
-                        Home
-                    </Button>
-                    {isAuthenticated ?
-                        (
+                    {/* Desktop */}
+                    <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+                        <Button component={Link} to="/" sx={{ color: 'white', fontWeight: "bold" }}>
+                            Home
+                        </Button>
+                        {isAuthenticated ? (
                             <>
-                                <Button component={Link} to="/notifications" sx={{ color: 'white', fontWeight: 'bold' }}>
+                                <Button component={Link} to="/notifications" sx={{ color: 'white', fontWeight: "bold" }}>
                                     Notifications
                                 </Button>
-                                <Button onClick={handleLogout} sx={{ color: 'white', fontWeight: 'bold' }}>
+                                <Button onClick={handleLogout} sx={{ color: 'white', fontWeight: "bold" }}>
                                     Logout
                                 </Button>
                             </>
-                            
                         ) : (
                             <>
-                                <Button component={Link} to="/register" sx={{ color: 'white', fontWeight: 'bold' }}>
+                                <Button component={Link} to="/register" sx={{ color: 'white', fontWeight: "bold" }}>
                                     Register
                                 </Button>
-                                <Button component={Link} to="/login" sx={{ color: 'white', fontWeight: 'bold' }}>
+                                <Button component={Link} to="/login" sx={{ color: 'white', fontWeight: "bold" }}>
                                     Login
                                 </Button>
                             </>
-                        )
-                   }
-                </Box>
+                        )}
+                    </Box>
 
-                {/* mobile menu icon*/}
-                <IconButton edge="end" color="inherit" aria-label="menu"
-                    onClick={handleMenuOpen}
-                    sx={{ display: { xs: 'block', md: 'none' } }} >
-                     <MenuIcon />
-                </IconButton>
+                    {/* Mobile Menu Icon */}
+                    <IconButton
+                        edge="end"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={handleMenuOpen}
+                        sx={{ display: { xs: 'block', md: 'none' } }}>
 
-                {/* mobile dropdown */}
-                <Menu anchorEl={anchorEl} open={Boolean(anchorEl)}
-                    onClose={handleMenueClose}
-                    sx={{
-                        display: { xs: 'block', md: 'none' },
-                        '& .MuiPaper-root': {
-                            minWidth: '250px', 
-                            padding: '10px', 
-                        },
-                    }}>
-                    <MenuItem onClick={handleMenueClose} component={Link} to="/">
-                        Home    
-                    </MenuItem>
-                    {isAuthenticated ?
-                        (
+                        <MenuIcon />
+                    </IconButton>
+
+                    {/* Mobile Dropdown Menu */}
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                            '& .MuiPaper-root': {
+                                minWidth: '250px', 
+                                padding: '10px',
+                            },
+                        }}
+                    >
+                        <MenuItem onClick={handleMenuClose} component={Link} to="/">
+                            Home
+                        </MenuItem>
+                        {isAuthenticated ? (
                             <>
-                                <MenuItem onClick={handleMenuClose} component={Link} to="/notification">
-                                    Notifications
-                                </MenuItem>
                                 <MenuItem onClick={handleLogout}>
                                     Logout
+                                </MenuItem>
+                                <MenuItem onClick={handleMenuClose} component={Link} to="/notifications">
+                                    Notifications
                                 </MenuItem>
                             </>
                         ) : (
@@ -114,13 +119,13 @@ const Navbar = () => {
                                 </MenuItem>
                                 <MenuItem onClick={handleMenuClose} component={Link} to="/register">
                                     Register
-                                </MenuItem> 
+                                </MenuItem>
                             </>
-                        )    
-                    }
-                </Menu>
-            </Toolbar>
-        </AppBar>
+                        )}
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+        </>
     );
 };
 
