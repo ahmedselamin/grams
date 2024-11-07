4b9886b6e34bba1bbb52495a2213024a67db584c
eddyiiz <eddyxsayf@gmail.com>
2024-11-07 10:33:47 +0200

    auth provider

--- grams.client/src/authContext.jsx
+++ grams.client/src/authContext.jsx
@@ -5,3 +5,12 @@ import axiosInstance from './utils/axiosInstance';
 
 //create context
 export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+    return (
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    )
+}
